7cb1ce2324a5b5da49fcc3a3e8b92ed633dfa6f8
Nathan Florea <florean@gmail.com>
2016-08-18 17:16:20 -0700

    Setup Twitter api.

--- ticket_bot.py
+++ ticket_bot.py
@@ -1,6 +1,7 @@
 #!/usr/local/virtualenvs/ds-py3/bin/python
 import json
 
+import tweepy
 
 SECRETS_FILE = "secrets.json"
 
@@ -9,6 +10,9 @@ def main():
     # Twitter API setup
     with open(SECRETS_FILE) as f:
         secrets = json.load(f)
+    auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
+    auth.set_access_token(secrets['access_token'], secrets['access_secret'])
+    api = tweepy.API(auth)
 
 
 if __name__ == '__main__':
