0e764448d84e3560fa760f9e12192f0360a1e49f
Nathan Florea <florean@gmail.com>
2016-08-18 17:19:00 -0700

    Load event JSON.

--- ticket_bot.py
+++ ticket_bot.py
@@ -1,6 +1,7 @@
 #!/usr/local/virtualenvs/ds-py3/bin/python
 import json
 
+import requests
 import tweepy
 
 SECRETS_FILE = "secrets.json"
@@ -13,6 +14,12 @@ def main():
     auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
     auth.set_access_token(secrets['access_token'], secrets['access_secret'])
     api = tweepy.API(auth)
+
+    # Loop forever
+    while True:
+        ticket_json = requests.get(TICKET_URL).json()
+        for event in ticket_json['times']:
+            pass
 
 
 if __name__ == '__main__':
