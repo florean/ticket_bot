9f7220f47defec9b2a1ee8a578319f2620c90150
Nathan Florea <florean@gmail.com>
2016-08-18 17:14:08 -0700

    Load secrets.json.

--- ticket_bot.py
+++ ticket_bot.py
@@ -1,8 +1,15 @@
 #!/usr/local/virtualenvs/ds-py3/bin/python
+import json
+
+
+SECRETS_FILE = "secrets.json"
 
 
 def main():
-    pass
+    # Twitter API setup
+    with open(SECRETS_FILE) as f:
+        secrets = json.load(f)
+
 
 if __name__ == '__main__':
     main()
